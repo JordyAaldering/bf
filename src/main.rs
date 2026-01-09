7c9cea24c7cf1d026bc524721b51639d4b42d97f
Jordy Aaldering <aalderingjordy@gmail.com>
2026-01-09 16:28:45 +0100

    todo

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ enum Instruction {
     IncPtr,
     /// `<`
     ///
-    /// Increment the data pointer by one.
+    /// Decrement the data pointer by one.
     DecPtr,
     /// `+`
     ///
@@ -16,8 +16,12 @@ enum Instruction {
     IncVal,
     /// `-`
     ///
-    /// Increment the byte at the data pointer by one.
+    /// Decrement the byte at the data pointer by one.
     DecVal,
+    /// `[-]` or `[+]`
+    ///
+    /// Reset the byte at the data pointer to zero.
+    ClearVal,
     /// `.`
     ///
     /// Output the byte at the data pointer.
@@ -91,6 +95,62 @@ fn parse(src: &str) -> Result<Vec<Instruction>, Error> {
     Ok(bf)
 }
 
+/// Cancel out adjacent increments and decrements
+///
+/// TODO: this does not work, because the pointers become invalid.
+/// Probably easiest if LoopOpen and LoopEnd become a recursive definition instead
+/// E.g., Loop(Vec<Instruction>)
+fn cancel(bf: &mut Vec<Instruction>) {
+    let mut i = 0;
+
+    while i + 1 < bf.len() {
+        let a = bf[i];
+        let b = bf[i + 1];
+
+        use Instruction::*;
+        match (a, b) {
+            (IncPtr, DecPtr) |
+            (DecPtr, IncPtr) |
+            (IncVal, DecVal) |
+            (DecVal, IncVal) => {
+                bf.remove(i + 1);
+                bf.remove(i);
+            }
+            _ => {
+                i += 1
+            },
+        }
+    }
+}
+
+/// Replace `[+]` and `[-]` by a single instruction.
+///
+/// TODO: this does not work, because the pointers become invalid.
+/// Probably easiest if LoopOpen and LoopEnd become a recursive definition instead
+/// E.g., Loop(Vec<Instruction>)
+fn clearloop(bf: &mut Vec<Instruction>) {
+    let mut i = 0;
+
+    while i + 2 < bf.len() {
+        let a = bf[i];
+        let b = bf[i + 1];
+        let c = bf[i + 2];
+
+        use Instruction::*;
+        match (a, b, c) {
+            (LoopOpen(_), IncVal, LoopEnd(_)) |
+            (LoopOpen(_), DecVal, LoopEnd(_)) => {
+                bf[i] = ClearVal;
+                bf.remove(i + 2);
+                bf.remove(i + 1);
+            }
+            _ => {
+                i += 1;
+            }
+        }
+    }
+}
+
 fn eval(bf: &Vec<Instruction>, rdr: &mut impl Read, wtr: &mut impl Write) -> io::Result<()> {
     let mut tape = [0u8; 64];
     let mut ptr = 0;
@@ -103,6 +163,7 @@ fn eval(bf: &Vec<Instruction>, rdr: &mut impl Read, wtr: &mut impl Write) -> io:
             DecPtr => ptr -= 1,
             IncVal => tape[ptr] = tape[ptr].wrapping_add(1),
             DecVal => tape[ptr] = tape[ptr].wrapping_sub(1),
+            ClearVal => tape[ptr] = 0,
             Write  => {
                 wtr.write(&tape[ptr..=ptr])?;
             },
@@ -133,9 +194,15 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let src = fs::read_to_string(&args[1])
         .map_err(|e| e.to_string())?;
-    let bf = parse(&src)
+    let mut bf = parse(&src)
         .map_err(|e| e.to_string())?;
+
+    // Optimize
+    cancel(&mut bf);
+    clearloop(&mut bf);
+
     eval(&bf, &mut io::stdin(), &mut io::stdout())
         .map_err(|e| e.to_string())?;
+
     Ok(())
 }
